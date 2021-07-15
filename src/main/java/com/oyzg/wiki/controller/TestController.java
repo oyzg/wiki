d9d3d2311f6ba1d601ab6f388458a371f5cd6280
oyzg <2317437823@qq.com>
2021-07-15 10:37:12 +0800

    2-6 开发hello World接口

---
+++ src/main/java/com/oyzg/wiki/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.oyzg.wiki.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//返回字符串
+//@Controller//返回页面
+public class TestController {
+
+    @GetMapping("/hello")
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello world!";
+    }
+
+}
