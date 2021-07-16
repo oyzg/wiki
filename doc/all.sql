61194cead8c1c2c7e6203b05c73869612bda87f5
oyzg <2317437823@qq.com>
2021-07-16 11:13:58 +0800

    3-5 集成mybatis generator

--- doc/all.sql
+++ doc/all.sql
@@ -7,3 +7,10 @@ CREATE table `test` (
 ) engine=InnoDB default charset=utf8mb4 comment='测试';
 
 insert into `test` (id,name,password) values(1,'测试','password');
+
+drop table if exists `dome`;
+CREATE table `dome` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment '名称',
+                        primary key (`id`)
+) engine=InnoDB default charset=utf8mb4 comment='测试';
