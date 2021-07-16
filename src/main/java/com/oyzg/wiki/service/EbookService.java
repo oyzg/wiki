b6133eb3c6ff753efdaff41f438b0f7ee7541ed7
oyzg <2317437823@qq.com>
2021-07-16 14:56:31 +0800

    3-8 增加CopyUtil工具类封装BeanUtils

--- src/main/java/com/oyzg/wiki/service/EbookService.java
+++ src/main/java/com/oyzg/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.oyzg.wiki.domain.EbookExample;
 import com.oyzg.wiki.mapper.EbookMapper;
 import com.oyzg.wiki.req.EbookReq;
 import com.oyzg.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.oyzg.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,12 +22,15 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%"+req.getName()+"%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<EbookResp>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList = new ArrayList<EbookResp>();
+//        for (Ebook ebook : ebookList) {
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook,ebookResp);
+//            //对象复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+        List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
         return respList;
     }
 
