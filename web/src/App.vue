68aa2226ad93008d875b2b96bb8ecee83bdd837e
oyzg <2317437823@qq.com>
2021-07-20 21:18:34 +0800

    5-4 修正首页布局，加入Ant Design Vue list组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <a-layout id="components-layout-demo-top-side-2">
-    <the-header></the-header>
+  <a-layout >
+    <the-header/>
     <router-view/>
-    <the-footer></the-footer>
+    <the-footer/>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 import TheHeader from '@/components/the-header.vue';
 import TheFooter from '@/components/the-footer.vue';
-
 export default defineComponent({
   name: 'app',
   components: {
     TheHeader,
-    TheFooter,
-  },
+    TheFooter
+  }
 });
 </script>
 
-
 <style>
 #components-layout-demo-top-side-2 .logo {
+  float: left;
   width: 120px;
   height: 31px;
-  background: rgba(255, 255, 255, 0.2);
-  margin: 16px 28px 16px 0;
-  float: left;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+.site-layout-background {
+  background: #fff;
 }
 </style>
