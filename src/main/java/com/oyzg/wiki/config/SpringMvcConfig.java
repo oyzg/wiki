b90189c330b2df0fcbcc5fdcedb629a6f87957a8
oyzg <2317437823@qq.com>
2021-07-20 22:45:03 +0800

    5-8 SpringBoot拦截器打印接口耗时

---
+++ src/main/java/com/oyzg/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,23 @@
+package com.oyzg.wiki.config;
+
+import com.oyzg.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login");
+    }
+
+}
--- src/main/java/com/oyzg/wiki/filter/LogFilter.java
+++ src/main/java/com/oyzg/wiki/filter/LogFilter.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Component
@@ -21,13 +20,13 @@ public class LogFilter implements Filter {
      @Override
      public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
          // 打印请求信息
-         HttpServletRequest request = (HttpServletRequest) servletRequest;
-         LOG.info("------------- LogFilter 开始 -------------");
-         LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
-         LOG.info("远程地址: {}", request.getRemoteAddr());
-
-         long startTime = System.currentTimeMillis();
-         filterChain.doFilter(servletRequest, servletResponse);
-         LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+//         HttpServletRequest request = (HttpServletRequest) servletRequest;
+//         LOG.info("------------- LogFilter 开始 -------------");
+//         LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+//         LOG.info("远程地址: {}", request.getRemoteAddr());
+//
+//         long startTime = System.currentTimeMillis();
+//         filterChain.doFilter(servletRequest, servletResponse);
+//         LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
      }
 }
