1652a0c01f39da5d5e36d81afd0016b8f3899325
oyzg <2317437823@qq.com>
2021-07-21 09:21:02 +0800

    5-9 SpringBoot AOP 打印接口耗时

--- src/main/java/com/oyzg/wiki/filter/LogFilter.java
+++ src/main/java/com/oyzg/wiki/filter/LogFilter.java
@@ -1,25 +1,26 @@
-package com.oyzg.wiki.filter;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.*;
-import java.io.IOException;
-
-@Component
-public class LogFilter implements Filter {
-
-    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
-
-     @Override
-     public void init(FilterConfig filterConfig) throws ServletException {
-
-     }
-
-     @Override
-     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-         // 打印请求信息
+//package com.oyzg.wiki.filter;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//
+//@Component
+//public class LogFilter implements Filter {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+//
+//     @Override
+//     public void init(FilterConfig filterConfig) throws ServletException {
+//
+//     }
+//
+//     @Override
+//     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//         // 打印请求信息
 //         HttpServletRequest request = (HttpServletRequest) servletRequest;
 //         LOG.info("------------- LogFilter 开始 -------------");
 //         LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
@@ -28,5 +29,5 @@ public class LogFilter implements Filter {
 //         long startTime = System.currentTimeMillis();
 //         filterChain.doFilter(servletRequest, servletResponse);
 //         LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
-     }
-}
+//     }
+//}
