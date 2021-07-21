d3823fbfd0d716207a77abbd7e340fd7355adacc
oyzg <2317437823@qq.com>
2021-07-21 15:15:08 +0800

    6-4 使用PageHelper实现后端分页

--- src/main/java/com/oyzg/wiki/service/EbookService.java
+++ src/main/java/com/oyzg/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.oyzg.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.oyzg.wiki.domain.Ebook;
 import com.oyzg.wiki.domain.EbookExample;
 import com.oyzg.wiki.mapper.EbookMapper;
 import com.oyzg.wiki.req.EbookReq;
 import com.oyzg.wiki.resp.EbookResp;
 import com.oyzg.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -15,16 +19,27 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
+    
+
 
     public List<EbookResp> list(EbookReq req) {
+
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%"+req.getName()+"%");
         }
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数:{}",pageInfo.getTotal());
+        LOG.info("总页数:{}",pageInfo.getPages());
+
 //        List<EbookResp> respList = new ArrayList<EbookResp>();
 //        for (Ebook ebook : ebookList) {
 ////            EbookResp ebookResp = new EbookResp();
