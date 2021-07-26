4d61fffeeb6913df1002d7fe799109231fa85cfa
oyzg <2317437823@qq.com>
2021-07-26 21:56:03 +0800

    9-10 前端显示登录昵称 遗留问题：刷新会消失

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -22,7 +22,10 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" @click="showLoginModal">
+      <a class="login-menu" v-show="user.id">
+        <span>您好:{{user.name}}</span>
+      </a>
+      <a class="login-menu" @click="showLoginModal" v-show="!user.id">
         <span>登录</span>
       </a>
     </a-menu>
@@ -62,8 +65,12 @@ export default defineComponent({
     // -------- 登录 ---------
     const loginUser = ref({
       loginName: 'test',
-      password: 'test'
+      password: 'test123'
     });
+
+    //登录后
+    const user = ref();
+    user.value = {};
 
     const loginModalVisible = ref(false);
     const loginModalLoading = ref(false);
@@ -81,6 +88,7 @@ export default defineComponent({
         if (data.success) {
           loginModalLoading.value = false;
           message.success("登录成功！");
+          user.value = data.content;
         } else {
           message.error(data.message);
           loginModalLoading.value = false;
@@ -93,7 +101,8 @@ export default defineComponent({
       loginModalLoading,
       login,
       showLoginModal,
-      loginUser
+      loginUser,
+      user
     }
 
   }
