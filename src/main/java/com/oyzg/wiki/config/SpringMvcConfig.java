f53b4ba3f8f6e330c284f6204f334d44af3aa4ec
oyzg <2317437823@qq.com>
2021-07-27 11:19:20 +0800

    9-11 前后端增加登录拦截

--- src/main/java/com/oyzg/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/oyzg/wiki/config/SpringMvcConfig.java
@@ -1,4 +1,4 @@
-//package com.oyzg.wiki.config;
+package com.oyzg.wiki.config;
 //
 //import com.oyzg.wiki.interceptor.LogInterceptor;
 //import org.springframework.context.annotation.Configuration;
@@ -18,3 +18,32 @@
 //                .addPathPatterns("/**");
 //    }
 //}
+
+import com.oyzg.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/find-content/**"
+                );
+    }
+}
